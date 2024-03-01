d4b0fa07d407b6db43ca96c57ee10089eab1df94
rafaelarsh <161667518+rafaelarsh@users.noreply.github.com>
2024-03-01 10:30:02 +0700

    Add files via upload

---
+++ offline.js
@@ -0,0 +1,21 @@
+﻿{
+	"version": 1709263700,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-3.4.1.min.js",
+		"offlineClient.js",
+		"images/walpaperflappybird-sheet0.png",
+		"images/pngclipartflappybirdflappycheffspritebirdframegame-sheet0.png",
+		"images/plysheet-sheet0.png",
+		"images/pngclipartflappybirdflappycheffspritebirdframegame1removebgpreview-sheet0.png",
+		"images/pipabawah-sheet0.png",
+		"images/pipaatas-sheet0.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
